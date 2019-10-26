e5bd11aedc092ac444333a70e69297be6c075b13
skhan75 <sami.ahmadkhan12@gmail.com>
2019-10-26 16:08:27 -0700

    Using Dynamic data and adding styles

---
+++ react-udemy-complete-fullstack-course/my-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import Header from './components/header';
+
+// const App = () => {
+//   return (
+//     <div className="newElement">
+//       <h1> Hello React !! </h1>
+//       <div> Hey </div>
+//     </div>
+//   )
+// };
+
+ReactDOM.render(<Header />, document.getElementById('root'));
