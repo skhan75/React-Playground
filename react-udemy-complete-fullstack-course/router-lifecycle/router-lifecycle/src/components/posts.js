fb448803b04d866bae98cb4abaa84cbd50f18814
skhan75 <sami.ahmadkhan12@gmail.com>
2019-10-27 11:32:01 -0700

    Returning arrays

--- react-udemy-complete-fullstack-course/router-lifecycle/router-lifecycle/src/components/posts.js
+++ react-udemy-complete-fullstack-course/router-lifecycle/router-lifecycle/src/components/posts.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Posts = () => {
-  return (
-    <div>
-      <Link to="/posts/1">Post 1</Link><br/>
-      <Link to="/posts/2">Post 2</Link><br/>
-      <Link to="/posts/3">Post 3</Link>
-    </div>
-  )
+
+  const ids = [
+    {id:'1', name:'Post1'},
+    {id:'2', name:'Post2'},
+    {id:'3', name:'Post3'}
+  ];
+
+  // const list = ids.map(item => {
+  //   return (
+  //       <span key={item.id}>
+  //         <Link to={item.id}>{item.name}</Link><br/>
+  //       </span>
+  //   )
+  // });
+
+  // return (
+  //   <div>
+  //     {list}
+  //   </div>
+  // )
+  return [
+    <div key="1"> Hello </div>,
+    <div key="2"> I am </div>,
+    <div key="3"> a react app </div>
+  ]
 }
 
 export default Posts;
