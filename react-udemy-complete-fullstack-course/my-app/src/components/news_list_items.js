52cd65767f74405a35de07127c1681f9aba5ea50
skhan75 <sami.ahmadkhan12@gmail.com>
2019-10-26 18:56:07 -0700

    More on css

--- react-udemy-complete-fullstack-course/my-app/src/components/news_list_items.js
+++ react-udemy-complete-fullstack-course/my-app/src/components/news_list_items.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { css } from 'glamor';
+// import { css } from 'glamor';
+import classes from '../css/styles.css';
+
 
 const NewsItem = ({ item }) => {
 
-  let news_item = css({
-    padding: '20px',
-    boxSizing: 'border-box',
-    borderBottom: '1px solid grey',
-    ':hover': {
-      color: 'red'
-    },
-    '@media(max-width: 500px)': {
-      color: 'blue'
-    }
-  });
-
-  let item_grey = css({
-    background: 'lightgrey'
-  });
+  // let news_item = css({
+  //   padding: '20px',
+  //   boxSizing: 'border-box',
+  //   borderBottom: '1px solid grey',
+  //   ':hover': {
+  //     color: 'red'
+  //   },
+  //   '@media(max-width: 500px)': {
+  //     color: 'blue'
+  //   }
+  // });
+  //
+  // let item_grey = css({
+  //   background: 'lightgrey'
+  // });
 
   return (
-    <div {...news_item} {...item_grey}>
+    <div className={classes.news_item}>
         <h3>{item.title}</h3>
         <div>
           {item.feed}
